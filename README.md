9052bea6d2ee6c7ac79597cfe1bd7259256b8356
polint <polint@uw.edu>
2020-05-17 12:44:24 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a script to automatically process all images in a given folder,  
+performing some kind of image processing and adding the resulting  
+info to a fourth channel, saving the result to a new file
---
+++ script.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# TODO:
+#       - get list of filepaths for every image to be processed
+#       - loop through images, send each to function that processes them
+#         and returns the result
+#       - save result under modified filepath
+
+
+# list of every filepath
+#   - assume script is run at base directory of all images
+#       | - script
+#       | - image folder 1
+#       | - image folder 2
+#       | - results (if run before)
+#   - user-specified filetype for images (global variable)
+#     default = png
+
+import glob
+import os
+
+cwd = os.getcwd()
+files = glob.glob(cwd + '/**/*.png', recursive=True)
+print(files)
